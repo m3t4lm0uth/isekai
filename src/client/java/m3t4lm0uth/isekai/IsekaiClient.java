33e2716f29b9654a1fd0f5b4c80394a4807a0f39
m3t4lm0uth <htuom.latem@gmail.com>
2026-02-26 15:01:02 -0800

    Initial skeleton

---
+++ src/client/java/m3t4lm0uth/isekai/IsekaiClient.java
@@ -0,0 +1,10 @@
+package m3t4lm0uth.isekai;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IsekaiClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
